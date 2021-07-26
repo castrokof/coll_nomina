135d2aeaf2201e62ac3b95ccf11c96627deb7b4f
dbastidasn <dabastidas@gmail.com>
2021-07-26 15:34:01 -0500

    Cambio Titutulo

--- resources/views/seguridad/index.blade.php
+++ resources/views/seguridad/index.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Ips-Fidem | Log in</title>
+  <title>Registro de Turnos</title>
 
     <!-- Favicons -->
     <link href="{{asset("assets/img/fidem_icon.jpeg")}}" rel="icon">
